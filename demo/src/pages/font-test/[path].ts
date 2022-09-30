162c8ee9adaa0956d10471b58f4cd845d5333925
delucis <swithinbank@gmail.com>
2022-09-30 21:59:00 +0200

    Add more complex font loading example to demo project
    Uses some Astro docs content to test different languages & fonts

---
+++ demo/src/pages/font-test/[path].ts
@@ -0,0 +1,58 @@
+import { OGImageRoute } from 'astro-og-canvas';
+import { pages } from './_pages';
+
+export const { getStaticPaths, get } = OGImageRoute({
+  param: 'path',
+  pages,
+  getImageOptions: (_path, page) => ({
+    title: page.title,
+    description: page.description,
+    dir: page.dir,
+    logo: { path: './src/astro-docs-logo.png', size: [350] },
+    border: { width: 10 },
+    padding: 40,
+    font: {
+      title: {
+        families: [
+          'Work Sans',
+          'Noto Sans Black',
+          'Noto Sans Arabic',
+          'Noto Sans SC Black',
+          'Noto Sans TC Black',
+          'Noto Sans JP Black',
+        ],
+        weight: 'ExtraBold',
+      },
+      description: {
+        families: [
+          'Work Sans',
+          'Noto Sans',
+          'Noto Sans Arabic',
+          'Noto Sans SC',
+          'Noto Sans TC',
+          'Noto Sans JP',
+        ],
+        weight: 'Normal',
+      },
+    },
+    fonts: [
+      'https://api.fontsource.org/v1/fonts/work-sans/latin-400-normal.ttf',
+      'https://api.fontsource.org/v1/fonts/work-sans/latin-800-normal.ttf',
+
+      'https://api.fontsource.org/v1/fonts/noto-sans/cyrillic-400-normal.ttf',
+      'https://api.fontsource.org/v1/fonts/noto-sans/cyrillic-900-normal.ttf',
+
+      'https://api.fontsource.org/v1/fonts/noto-sans-sc/chinese-simplified-400-normal.ttf',
+      'https://api.fontsource.org/v1/fonts/noto-sans-sc/chinese-simplified-900-normal.ttf',
+
+      'https://api.fontsource.org/v1/fonts/noto-sans-tc/chinese-traditional-400-normal.ttf',
+      'https://api.fontsource.org/v1/fonts/noto-sans-tc/chinese-traditional-900-normal.ttf',
+
+      'https://api.fontsource.org/v1/fonts/noto-sans-jp/japanese-400-normal.ttf',
+      'https://api.fontsource.org/v1/fonts/noto-sans-jp/japanese-900-normal.ttf',
+
+      'https://api.fontsource.org/v1/fonts/noto-sans-arabic/arabic-400-normal.ttf',
+      'https://api.fontsource.org/v1/fonts/noto-sans-arabic/arabic-800-normal.ttf',
+    ],
+  }),
+});
---
+++ demo/src/pages/font-test/_pages.ts
@@ -0,0 +1,36 @@
+export const pages: Record<string, { title: string; description: string; dir?: 'rtl' | 'ltr' }> = {
+  'ar-editor-setup.md': {
+    title: 'إعداد البيئة البرمجية',
+    description: 'أعِد محرر الشفرة لبناء المشاريع مع Astro',
+    dir: 'rtl',
+  },
+  'markdown.md': {
+    title: 'Markdown & MDX',
+    description: 'Learn how to create content using Markdown or MDX with Astro',
+  },
+  'components.md': {
+    title: 'Components',
+    description: 'An intro to the .astro component syntax.',
+  },
+  'project-structure.md': {
+    title: 'Project Structure',
+    description: 'Learn how to structure a project with Astro.',
+  },
+  'why-astro.md': {
+    title: 'Why Astro?',
+    description:
+      'Astro is an all-in-one web framework for building fast, content-focused websites. Learn more.',
+  },
+  'install-auto.md': {
+    title: 'Install Astro with the Automatic CLI',
+    description: 'How to install Astro with NPM, PNPM, or Yarn via the create-astro CLI tool.',
+  },
+  'zh-editor-setup.md': {
+    title: '编辑器配置',
+    description: '配置与 Astro 一同使用的编辑器',
+  },
+  'ru-getting-started.md': {
+    title: 'Начало Работы',
+    description: 'Основное введение в Astro.',
+  },
+};
